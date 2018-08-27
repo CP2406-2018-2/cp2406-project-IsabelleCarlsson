6cf1bd9d05eca17e49ca738f39dd9aae3c6dd87f
Isabelle Carlsson <isabelle.carlsson@my.jcu.edu.au>
2018-08-27 19:31:52 +1000

    Add roomName variable & getName, getElectUsage, getRoomName methods

--- HomeSimulator/SmartDevice.java
+++ HomeSimulator/SmartDevice.java
@@ -2,17 +2,32 @@ public class SmartDevice {
     private String deviceName;
     private int electUsage;
     private boolean isActive;
+    private String roomName;
 
     public SmartDevice() {
         deviceName = "device";
         electUsage = 1;
         isActive = false;
+        roomName = "";
     }
 
-    public SmartDevice(String deviceName, int electUsage, boolean isActive) {
+    public SmartDevice(String deviceName, String roomName, int electUsage, boolean isActive) {
         this.deviceName = deviceName;
         this.electUsage = electUsage;
         this.isActive = isActive;
+        this.roomName = roomName;
+    }
+
+    public String getName() {
+        return deviceName;
+    }
+
+    public int getElectUsage() {
+        return electUsage;
+    }
+
+    public String getRoomName() {
+        return roomName;
     }
 
     public boolean isActive() {
